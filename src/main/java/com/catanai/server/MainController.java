ae016c0fee5388b6dc4078b6dabe011bb61f2de1
taylor-cox <taylor@coxintl.com>
2023-08-25 08:09:14 -0400

    cleaned everything

--- src/main/java/com/catanai/server/MainController.java
+++ src/main/java/com/catanai/server/MainController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 /**
  * Main endpoint for Catan AI webviewer.
  */
