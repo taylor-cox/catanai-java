2349079ba43ff99093f039ee22d63dbe00df6f06
taylor-cox <taylor@coxintl.com>
2023-08-28 11:59:48 -0400

    added way to add player's next move to game; wip

--- src/main/java/com/catanai/server/config/SocketCommandHandler.java
+++ src/main/java/com/catanai/server/config/SocketCommandHandler.java
@@ -3,10 +3,14 @@ package com.catanai.server.config;
 import com.catanai.server.model.Game;
 import com.catanai.server.model.player.DeterministicPlayer;
 import com.catanai.server.model.player.PlayerId;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.apache.commons.lang.NotImplementedException;
+import org.json.JSONArray;
 
 /**
  * Socket command handler.
@@ -14,6 +18,7 @@ import org.apache.commons.lang.NotImplementedException;
 public class SocketCommandHandler {
   String command;
   Game game;
+  List<DeterministicPlayer> players;
 
   /**
    * Handles command from websocket.
@@ -21,23 +26,37 @@ public class SocketCommandHandler {
    * @param command command to handle from websocket.
    * @return string of command output.
    */
-  public String handleCommand(String command) {
+  public String handleCommand(String command, String action) {
     switch (command) {
       case "newGame":
-        List<DeterministicPlayer> players = new ArrayList<DeterministicPlayer>();
-        players.add(new DeterministicPlayer(PlayerId.ONE));
-        players.add(new DeterministicPlayer(PlayerId.TWO));
-        players.add(new DeterministicPlayer(PlayerId.THREE));
-        players.add(new DeterministicPlayer(PlayerId.FOUR));
-        game = new Game(players);
-        return Arrays.deepToString(game.getCurrentGameState().toArray());
+        this.players = new ArrayList<DeterministicPlayer>();
+        this.players.add(new DeterministicPlayer(PlayerId.ONE));
+        this.players.add(new DeterministicPlayer(PlayerId.TWO));
+        this.players.add(new DeterministicPlayer(PlayerId.THREE));
+        this.players.add(new DeterministicPlayer(PlayerId.FOUR));
+        this.game = new Game(players);
+        return this.currentGameStateAsJSON();
       case "getCurrentGamestate":
-        return this.game.getCurrentGameState().toArray().toString();
+        return this.currentGameStateAsJSON();
       case "makeMove":
         throw new NotImplementedException("Not implemented");
+        DeterministicPlayer currentPlayer = (DeterministicPlayer) this.game.getCurrentPlayer();
+        JSONArray jsonArray = new JSONArray(action);
+        currentPlayer.setNextMoveMetadata();
+        // this.game.
         // break;
       default:
         return "";
+    }
+  }
+
+  private String currentGameStateAsJSON() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      return objectMapper.writeValueAsString(this.game.getCurrentGameState().toMap());
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+      return "ERROR";
     }
   }
 }
