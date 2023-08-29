880e26ba9390e6f9309c6021986d1300333f85e8
taylor-cox <taylor@coxintl.com>
2023-08-29 14:09:20 -0400

    added better controls for game websocket

--- ai/websockets/game_websockets.py
+++ ai/websockets/game_websockets.py
@@ -9,8 +9,29 @@ class GameWebSocketHandler:
   def __init__(self, connectionString: str):
     self.connectionString = connectionString
 
-  def runAction(self, action: dict) -> str:
-    actionString = json.dumps(action)
+  def newGame(self) -> str:
+    actionString = json.dumps({'command': 'newGame'})
+    self.ws.send(bytes(actionString, "utf-8"))
+    return self.ws.recv()
+
+  def getCurrentGamestate(self) -> str:
+    actionString = json.dumps({'command': 'getCurrentGamestate'})
+    self.ws.send(bytes(actionString, "utf-8"))
+    return self.ws.recv()
+
+  def makeMove(self) -> str:
+    actionString = json.dumps({'command': 'makeMove'})
+    self.ws.send(bytes(actionString, "utf-8"))
+    return self.ws.recv()
+
+  def addMove(self, action: list, playerID: str) -> str:
+    actionString = json.dumps(
+      {
+        'command': 'addPlayerMove',
+        'action': action,
+        "playerID": playerID
+      }
+    )
     self.ws.send(bytes(actionString, "utf-8"))
     return self.ws.recv()
 
