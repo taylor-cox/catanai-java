7deaf751eccad7db850192c2237f17b9024a7865
taylor-cox <taylor@coxintl.com>
2023-08-29 14:07:52 -0400

    rudementary logic for adding moves, making moves, adding to db

--- ai/main.py
+++ ai/main.py
@@ -12,14 +12,14 @@ def run_training():
   n_epochs = 4
   alpha = 0.0003
   agent = Agent(
-    n_actions = env.action_space.n, 
+    n_actions = env.action_space.n,  # type: ignore
     batch_size=batch_size,
     alpha=alpha,
     n_epochs=n_epochs,
     input_dims=env.observation_space.shape
   )
 
-  print(env.action_space.n)
+  print(env.action_space.n) # type: ignore
 
   n_games = 300
 
@@ -60,9 +60,32 @@ def run_training():
     print(f'--------------------------\nEpisode: {i}\nScore: {score}\nAverage Score: {avg_score}\nTime steps: {n_steps}\nLearning steps: {learn_iters}')
 
 if __name__ == '__main__':
-  gameStateDao = GameStateDAO()
-  with GameWebSocketHandler('ws://192.168.1.108:8080/game') as websocketHandler:
-    actionResponse = websocketHandler.runAction(
-      {'command': 'newGame'}
-    )
-    gameStateDao.addGamestate(actionResponse, 0)
+  # gameStateDao = GameStateDAO()
+  # with GameWebSocketHandler('ws://192.168.1.108:8080/game') as websocketHandler:
+  #   gamestateString = websocketHandler.newGame()
+  #   gameStateDao.addGamestate(gamestateString, 0)
+  #   allPlayersMoves = [
+  #     {'1': [ [2, 0],  [1, 0],  [2, 2],  [1, 4]]},
+  #     {'2': [ [2, 7],  [1, 10], [2, 10], [1, 17]]},
+  #     {'3': [ [2, 16], [1, 23], [2, 20], [1, 32]]},
+  #     {'4': [ [2, 33], [1, 49], [2, 37], [1, 53]]},
+  #   ]
+
+  #   for player in allPlayersMoves:
+  #     for id in player:
+  #       for move in player[id]:
+  #         output = websocketHandler.addMove(playerID=id, action=move)
+  #         if json.loads(output)['success'] != True:
+  #           print(f'Error: {output}')
+    
+  #   for _ in range(16):
+  #     gamestateString = websocketHandler.makeMove()
+  #     gameStateDao.addGamestate(gamestateString, 0)
+  pass
+
+  # TODO
+  # 1. Create 4 actors for the game.
+  # 2. Create new game.
+  # 3. Ask actor for move, add move to corresponding player.
+  # 4. Execute move (makeMove).
+  #  
