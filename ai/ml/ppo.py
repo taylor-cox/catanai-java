d12e1f58b2df89d70a1c489d63d8248ebd25e590
taylor-cox <taylor@coxintl.com>
2023-09-03 15:29:33 -0400

    changed location of ppo.py, and updated for catan

---
+++ ai/ml/ppo.py
@@ -0,0 +1,315 @@
+import os
+from typing import List, Tuple
+import numpy as np
+import torch as T
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions.categorical import Categorical
+from torch.types import Number
+
+class PPOMemory:
+  def __init__(self, batch_size: int) -> None:
+    self.states = []
+    self.probs = []
+    self.vals = []
+    self.actions = []
+    self.rewards = []
+    self.dones = []
+  
+    self.batch_size = batch_size
+  
+  def generate_batches(
+      self
+    ) -> Tuple[
+      np.ndarray,
+      np.ndarray,
+      np.ndarray,
+      np.ndarray,
+      np.ndarray,
+      np.ndarray,
+      List[np.ndarray]
+    ]:
+    n_states = len(self.states)
+    batch_start = np.arange(0, n_states, self.batch_size)
+    indices = np.arange(n_states, dtype=np.int64)
+    np.random.shuffle(indices)
+    batches = [indices[i:i+self.batch_size] for i in batch_start]
+
+    return np.array(self.states), \
+      np.array(self.actions), \
+      np.array(self.probs), \
+      np.array(self.vals), \
+      np.array(self.rewards), \
+      np.array(self.dones), \
+      batches
+  
+  def store_memory(
+      self,
+      state,
+      action,
+      probs,
+      val,
+      reward,
+      done
+    ) -> None:
+    self.states.append(state)
+    self.actions.append(action)
+    self.probs.append(probs.cpu())
+    self.vals.append(val.tolist()[0])
+    self.rewards.append(reward)
+    self.dones.append(done)
+  
+  def clear_memory(self):
+    self.states = []
+    self.actions = []
+    self.probs = []
+    self.vals = []
+    self.rewards = []
+    self.dones = []
+
+class ActorNetwork(nn.Module):
+  def __init__(self, n_actions, input_dims: Tuple[int], alpha, chkpt_dir='tmp/ppo') -> None:
+    super(ActorNetwork, self).__init__()
+
+    self.checkpoint_file = os.path.join(chkpt_dir, 'actor_torch_ppo')
+    '''MODEL 1'''
+    self.network_to_use = nn.Sequential(
+      nn.Linear(*input_dims, 512),
+      nn.ReLU(),
+      nn.Linear(512, 512),
+      nn.ReLU(),
+      nn.Linear(512, 512),
+      nn.ReLU(),
+      nn.Linear(512, 512),
+      nn.ReLU(),
+      nn.Linear(512, 512),
+      nn.ReLU(),
+      # nn.Softmax(dim=-1)
+    ).to("cuda:0")
+    '''MODEL 2'''
+    # self.encoder = nn.TransformerEncoder(
+    #     nn.TransformerEncoderLayer(*input_dims, 11), num_layers=6
+    #   ).to("cuda:0")
+    
+    # self.decoder = nn.TransformerDecoder(
+    #     nn.TransformerDecoderLayer(*input_dims, 11), num_layers=6
+    #   ).to("cuda:0")
+    
+    # self.linear = nn.Linear(*input_dims, 11).to("cuda:0")
+    # self.relu = nn.ReLU().to("cuda:0")
+
+    self.policy_outputs = nn.ModuleList(
+      [
+        nn.Linear(512, 14),
+        nn.Linear(512, 72),
+        nn.Linear(512, 72),
+        nn.Linear(512, 20),
+        nn.Linear(512, 20),
+        nn.Linear(512, 20),
+        nn.Linear(512, 20),
+        nn.Linear(512, 20),
+        nn.Linear(512, 20),
+        nn.Linear(512, 20),
+        nn.Linear(512, 20),
+      ]
+    )
+
+    self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+    self.device = T.device("cuda:0" if T.cuda.is_available() else 'cpu')
+    self.to(self.device)
+
+  def forward(self, state):
+    # encoder_outputs = self.encoder(state)
+    # decoder_outputs = self.decoder(encoder_outputs, state)
+    # logits = self.linear(decoder_outputs)
+    # logits = self.relu(logits) 
+    logits = self.network_to_use(state)
+    policy_outputs = [T.softmax(head(logits), dim=-1) for head in self.policy_outputs]
+    policy_outputs = [Categorical(logits=head) for head in policy_outputs]
+    # print(logits)
+    return policy_outputs
+  
+  def save_checkpoint(self):
+    T.save(self.state_dict(), self.checkpoint_file)
+
+  def load_checkpoint(self):
+    self.load_state_dict(T.load(self.checkpoint_file))
+  
+class CriticNetwork(nn.Module):
+  def __init__(self, input_dims: Tuple[int], alpha, chkpt_dir='tmp/ppo'):
+    super(CriticNetwork, self).__init__()
+    self.checkpoint_file = os.path.join(chkpt_dir, 'actor_torch_ppo')
+    '''MODEL 1'''
+    self.network_to_use = nn.Sequential(
+      nn.Linear(*input_dims, 512),
+      nn.ReLU(),
+      nn.Linear(512, 256),
+      nn.ReLU(),
+      nn.Linear(256, 128),
+      nn.ReLU(),
+      nn.Linear(128, 64),
+      nn.ReLU(),
+      nn.Linear(64, 32),
+      nn.ReLU(),
+      nn.Linear(32, 1),
+      # nn.Softmax(dim=-1)
+    ).to("cuda:0")
+
+    '''MODEL 2'''
+    # self.encoder = nn.TransformerEncoder(
+    #     nn.TransformerEncoderLayer(*input_dims, 11), num_layers=6
+    #   ).to("cuda:0")
+    
+    # self.decoder = nn.TransformerDecoder(
+    #     nn.TransformerDecoderLayer(*input_dims, 11), num_layers=6
+    #   ).to("cuda:0")
+    
+    # self.linear = nn.Linear(*input_dims, 11).to("cuda:0")
+    # self.sigmoid = nn.Sigmoid().to("cuda:0")
+
+    self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+    self.device = T.device("cuda:0" if T.cuda.is_available() else 'cpu')
+    self.to(self.device)
+  
+  def forward(self, state):
+    # encoder_outputs = self.encoder(state)
+    # decoder_outputs = self.decoder(encoder_outputs, state)
+    # logits = self.linear(decoder_outputs)
+    logits = self.network_to_use(state)
+    return logits
+  
+  def save_checkpoint(self):
+    T.save(self.state_dict(), self.checkpoint_file)
+
+  def load_checkpoint(self):
+    self.load_state_dict(T.load(self.checkpoint_file))
+
+class Agent:
+  def __init__(
+      self, 
+      n_actions: int,
+      input_dims: Tuple[int] | None,
+      gamma: float = 0.99,
+      alpha: float = 0.0003,
+      gae_lambda: float = 0.95,
+      policy_clip: float = 0.2,
+      batch_size: int = 64,
+      N: int = 2048,
+      n_epochs: int = 10,
+      agent_id: str = '1'
+    ) -> None:
+    if input_dims == None:
+      raise Exception("input_dims must be specified")
+
+    self.gamma: float = gamma
+    self.policy_clip: float = policy_clip
+    self.n_epochs: int = n_epochs
+    self.gae_lambda: float = gae_lambda
+    self.agent_id: str = agent_id
+
+    self.actor: ActorNetwork = ActorNetwork(n_actions, input_dims, alpha)
+    self.critic: CriticNetwork = CriticNetwork(input_dims, alpha)
+    self.memory: PPOMemory = PPOMemory(batch_size)
+
+  def remember(self, state, action, probs, vals, reward, done):
+    self.memory.store_memory(state, action, probs, vals, reward, done)
+  
+  def save_models(self):
+    print('Saving models......')
+    self.actor.save_checkpoint()
+    self.critic.save_checkpoint()
+  
+  def load_models(self):
+    print("Loading models.......")
+    self.actor.load_checkpoint()
+    self.critic.load_checkpoint()
+  
+  # def choose_action(self, observation) -> Tuple[T.Tensor, T.Tensor, T.Tensor]:
+  def choose_action(self, observation) -> Tuple[T.Tensor, T.Tensor, T.Tensor]:
+    state = T.tensor([observation], dtype=T.float).to(self.actor.device)
+
+    dists: List[Categorical] = self.actor(state)
+    value: T.Tensor = self.critic(state)
+    action: T.Tensor = T.tensor([T.squeeze(dist.sample()).item() for dist in dists]).to(self.actor.device)
+    # probs = T.stack(dist.log_prob(action) for dist in dists).to(self.actor.device)
+    probs: T.Tensor = T.tensor([dist.log_prob(action[i]).item() for i, dist in enumerate(dists)]).to('cpu')
+    
+    return action, probs, value
+  
+  def learn(self):
+    for _ in range(self.n_epochs):
+      state_arr, action_arr, old_probs_arr, vals_arr, reward_arr, done_arr, batches = self.memory.generate_batches()
+      values = vals_arr
+      advantage = np.zeros(len(reward_arr), dtype=np.float32)
+
+      for t in range(len(reward_arr) - 1):
+        discount = 1
+        a_t = 0
+        for k in range(t, len(reward_arr) - 1):
+          a_t += discount * (reward_arr[k] + self.gamma * values[k+1] * (1-int(done_arr[k])) - values[k])
+          discount *= self.gamma * self.gae_lambda
+        advantage[t] = a_t
+      advantage = T.tensor(advantage).to(self.actor.device)
+
+      # print(advantage)
+
+      values = T.tensor(values).to(self.actor.device)
+      for batch in batches:
+        states = T.tensor(state_arr[batch], dtype=T.float).to(self.actor.device)
+        old_probs = T.tensor(old_probs_arr[batch]).to(self.actor.device)
+        actions = T.tensor(action_arr[batch]).to(self.actor.device)
+        # print("======================================")
+        # print(states)
+        # print(old_probs)
+        # print(actions)
+        # print("======================================")
+        # input("States, old_probs, actions")
+
+        dist = self.actor(states)
+        critic_value = self.critic(states)
+        # print("======================================")
+        # print(dist)
+        # print(critic_value)
+        # print("======================================")
+        # input("dist, critic_value")
+
+        critic_value = T.squeeze(critic_value)
+        new_probs = []
+
+        for i in range(11):
+          new_probs.append([])
+          for j in range(11):
+            new_probs[i].append(dist[j].log_prob(actions[i][j])[0])
+            
+        new_probs = T.stack([T.stack(new_p) for new_p in new_probs]).to(self.actor.device)
+
+        # print("======================================")
+        # print(new_probs)
+        # print(old_probs)
+        # print("======================================")
+        # input("new_probs, old_probs")
+        prob_ratio = new_probs.exp() / old_probs.exp()
+        # print("======================================")
+        # print(prob_ratio)
+        # print(advantage[batch])
+        # print(batch)
+        # print(advantage)
+        # print("======================================")
+        # input()
+        weighted_probs = advantage[batch] * prob_ratio
+        weighted_clipped_probs = T.clamp(prob_ratio, 1-self.policy_clip, 1+self.policy_clip)*advantage[batch]
+        actor_loss = -T.min(weighted_probs, weighted_clipped_probs).mean()
+
+        returns = advantage[batch] + values[batch]
+
+        critic_loss = (returns-critic_value)**2
+        critic_loss = critic_loss.mean()
+
+        total_loss = actor_loss + 0.5*critic_loss
+        self.actor.optimizer.zero_grad()
+        self.critic.optimizer.zero_grad()
+        total_loss.backward()
+        self.actor.optimizer.step()
+        self.critic.optimizer.step()
+    
+    self.memory.clear_memory()
