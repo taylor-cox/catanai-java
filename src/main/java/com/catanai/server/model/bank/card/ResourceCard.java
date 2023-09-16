62e4b9a15514e832e563174e6ee9721dba367551
taylor-cox <taylor@coxintl.com>
2023-09-16 15:51:59 -0400

    resource cards now in order

--- src/main/java/com/catanai/server/model/bank/card/ResourceCard.java
+++ src/main/java/com/catanai/server/model/bank/card/ResourceCard.java
@@ -7,11 +7,11 @@ import java.util.Optional;
 * Represents a resource card.
 */
 public enum ResourceCard implements Card {
-  LUMBER(1),
-  WOOL(2),
-  GRAIN(3),
-  BRICK(4),
-  ORE(5);
+  WOOL(0),
+  GRAIN(1),
+  LUMBER(2),
+  ORE(3),
+  BRICK(4);
   
   private final int value;
 
@@ -29,9 +29,13 @@ public enum ResourceCard implements Card {
    * @param value value associated with resource card
    * @return a resource card if value is associated with res card; else null.
    */
-  public static Optional<ResourceCard> valueOf(int value) {
-    return Arrays.stream(values())
-    .filter(resource -> resource.value == value)
-    .findFirst();
+  public static ResourceCard valueOf(int value) {
+    if (value < 0 || value > 4) {
+      return null;
+    }
+    Optional<ResourceCard> rc = Arrays.stream(values())
+            .filter(resource -> resource.value == value)
+            .findFirst();
+      return rc.orElse(null);
   }
 }
