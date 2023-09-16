fd3b8b95d62e3dc6bd3f3e3111f6f2b018caceca
taylor-cox <taylor@coxintl.com>
2023-09-16 15:43:02 -0400

    fixed end turn executor

--- src/main/java/com/catanai/server/model/action/executors/EndTurnExecutor.java
+++ src/main/java/com/catanai/server/model/action/executors/EndTurnExecutor.java
@@ -1,5 +1,6 @@
 package com.catanai.server.model.action.executors;
 
+import com.catanai.server.model.Game;
 import com.catanai.server.model.action.ActionMetadata;
 import com.catanai.server.model.action.ActionState;
 import com.catanai.server.model.player.Player;
@@ -9,6 +10,9 @@ import org.jetbrains.annotations.NotNull;
  * Class which validates and executes end turn action in game of Catan.
  */
 public class EndTurnExecutor implements SpecificActionExecutor {
+  public EndTurnExecutor(Game game) {
+  }
+
   @Override
   public boolean execute(@NotNull ActionMetadata amd, @NotNull Player p, @NotNull ActionState currentActionState) {
     if (currentActionState == ActionState.BUSINESS_AS_USUAL) {
