16dd3e72e61b021837985ad4c2e4c6fc2e364465
taylor-cox <taylor@coxintl.com>
2023-09-16 15:51:09 -0400

    added playerID (vs playerId)

---
+++ src/main/java/com/catanai/server/model/player/PlayerID.java
@@ -0,0 +1,47 @@
+package com.catanai.server.model.player;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Represents the ID of a Catan player.
+ */
+public enum PlayerID {
+  ONE(1),
+  TWO(2),
+  THREE(3),
+  FOUR(4);
+  
+  private final int value;
+
+  private PlayerID(int value) {
+    this.value = value;
+  }
+  
+  public int getValue() {
+    return this.value;
+  }
+  
+  @Override
+  public String toString() {
+    return Integer.toString(this.value);
+  }
+  
+  /**
+   * Get the player id associated with the value @param value.
+   *
+   * @param value value associated with player id
+   * @return player id associated with value; null if no associated player id
+   */
+  public static PlayerID valueOf(int value) {
+    List<PlayerID> possiblePlayerIds = Arrays.stream(values())
+        .filter(resource -> resource.value == value)
+        .collect(Collectors.toList());
+    if (possiblePlayerIds.size() <= 0) {
+      return null;
+    } else {
+      return possiblePlayerIds.get(0);
+    }
+  }
+}
