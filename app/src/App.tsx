683e8760a6c197588f1f1aee9a653203c92d8182
taylor-cox <taylor@coxintl.com>
2023-09-25 10:24:37 -0400

    left/right buttons to ui under board

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import './App.css';
 import Board from './components/Board/Board';
-import Navbar, { NavPairArray } from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import { Button, Tooltip, Space } from 'antd';
 
 function App() {
-  let navs: NavPairArray = [];
-  navs.push({name: 'Home', url: '/home'});
-  navs.push({name: 'Database', url: '/home'});
-  navs.push({name: 'Terms and Conditions', url: '/home'});
   return (
     <div className="App">
-      <Navbar navOptions={navs}></Navbar>
-      <Board tiles={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}></Board>
+      <div id="app-container">
+        <Navbar />
+        <div id="board-container">
+          <Board></Board>
+          <div id="scroll-turns">
+            <Button shape="circle" icon={<LeftOutlined />} size="large"/>
+            <Button shape="circle" icon={<RightOutlined />} size="large"/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
