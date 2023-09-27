6b2b5c3d51b969b9c2a9ae83e0ccb66ac072751e
taylor-cox <taylor@coxintl.com>
2023-09-27 14:50:21 -0400

    added routes for each nav item

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Board from './components/Board/Board';
+import BoardView from './components/BoardView/BoardView';
 import Navbar from './components/Navbar/Navbar';
 import { NavigateFunction, Route, Routes, useNavigate } from 'react-router-dom';
 import { useAppSelector } from './hooks';
@@ -10,15 +10,21 @@ function App() {
 
   // Navigation for Navbar
   const navigate: NavigateFunction = useNavigate();
+  if (window.location.href.split('/').pop() !== page.value) {
+    console.log(window.location.href.split('/'))
+    navigate(page.value);
+  }
 
-  navigate(page.value);
   return (
     <div className="App">
       <div id="app-container">
         <Navbar />
         <div id="changing-container">
           <Routes>
-            <Route path='/' element={<Board />} />
+            <Route path='/' element={<BoardView />} />
+            <Route path='/statistics' element={<div>Statistics</div>} />
+            <Route path='/database' element={<div>Database</div>} />
+            <Route path='/settings' element={<div>Settings</div>} />
           </Routes>
         </div>
       </div>
