aee8970490851e6a07bc30f69833d04edd3def3c
taylor-cox <taylor@coxintl.com>
2023-09-27 14:49:15 -0400

    moved player to board view

--- app/src/components/BoardView/Player/Player.tsx
+++ app/src/components/BoardView/Player/Player.tsx
@@ -9,6 +9,11 @@ import RoadPNG from '../../../icons/road.png';
 import CityPNG from '../../../icons/city.png';
 import HomePNG from '../../../icons/home.png';
 import StarPNG from '../../../icons/star.png';
+import LastActionPNG from '../../../icons/last-action.png';
+import KnightPNG from '../../../icons/knight.png';
+import LongestRoadPNG from '../../../icons/longest-road.png';
+import ArmyPNG from '../../../icons/army.png';
+
 import {playerColors} from '../BoardView';
 
 
@@ -30,7 +35,7 @@ const starIconStyle = {
 }
 
 const pngIconStyle = {
-  width: '1.4rem',
+  width: '1rem',
   paddingLeft: '0.4rem',
   paddingRight: '0.4rem'
 }
@@ -59,11 +64,16 @@ const Player: React.FC<PlayerProps> = (playerProps) => {
         <UserOutlined style={{
           color: playerColors[playerID],
           backgroundColor: playerIconBackgroundColor,
-          padding: '0.2rem', borderRadius: "10%"
+          padding: '0.2rem', borderRadius: "0.25rem",
+          marginRight: '0.4rem',
+          minWidth: '1rem'
         }}/>
+        <p>{playerID}</p>
       </div>
       <div id="player-current-state">
+        {/* ----------- Player Victory Points + Buildings Left ------------- */}
         <div id="player-victory-points-buildings">
+          <p style={{textDecoration: 'underline'}}>Points / Buildings</p>
           <div id="player-vp" title="Victory Points">
             <img src={StarPNG} style={starIconStyle} alt="player victory points" id="star-png"/>
             <p>10</p>
@@ -81,30 +91,76 @@ const Player: React.FC<PlayerProps> = (playerProps) => {
             <p>5</p>
           </div>
         </div>
+        {/* ------------------------ Player Resources ---------------------- */}
         <div id="player-resources">
+          <p style={{textDecoration: 'underline'}}>Resources</p>
           <div id="player-brick" title="Brick">
-            <img src={BrickPNG} style={pngIconStyle} alt="player victory points" id="brick-png"/>
+            <img src={BrickPNG} style={pngIconStyle} alt="player brick amount" id="brick-png"/>
             <p>1</p>
           </div>
           <div id="player-wood" title="Wood">
-            <img src={WoodPNG} style={pngIconStyle} alt="player victory points" id="wood-png"/>
+            <img src={WoodPNG} style={pngIconStyle} alt="player wood amount" id="wood-png"/>
             <p>1</p>
           </div>
           <div id="player-wheat" title="Wheat">
-            <img src={WheatPNG} style={pngIconStyle} alt="player victory points" id="wheat-png"/>
+            <img src={WheatPNG} style={pngIconStyle} alt="player wheat amount" id="wheat-png"/>
             <p>1</p>
           </div>
           <div id="player-sheep" title="Sheep">
-            <img src={SheepPNG} style={pngIconStyle} alt="player victory points" id="sheep-png"/>
+            <img src={SheepPNG} style={pngIconStyle} alt="player sheep amount" id="sheep-png"/>
             <p>1</p>
           </div>
           <div id="player-ore" title="Ore">
-            <img src={OrePNG} style={pngIconStyle} alt="player victory points" id="ore-png"/>
+            <img src={OrePNG} style={pngIconStyle} alt="player ore amount" id="ore-png"/>
             <p>1</p>
           </div>
         </div>
+        {/* ----------------------- Player Dev Cards ----------------------- */}
+        <div id="player-resources">
+          <p style={{textDecoration: 'underline'}}>Dev. Cards</p>
+          <div id="player-brick" title="Brick">
+            <img src={BrickPNG} style={pngIconStyle} alt="player brick amount" id="brick-png"/>
+            <p>1</p>
+          </div>
+          <div id="player-wood" title="Wood">
+            <img src={WoodPNG} style={pngIconStyle} alt="player wood amount" id="wood-png"/>
+            <p>1</p>
+          </div>
+          <div id="player-wheat" title="Wheat">
+            <img src={WheatPNG} style={pngIconStyle} alt="player wheat amount" id="wheat-png"/>
+            <p>1</p>
+          </div>
+          <div id="player-sheep" title="Sheep">
+            <img src={SheepPNG} style={pngIconStyle} alt="player sheep amount" id="sheep-png"/>
+            <p>1</p>
+          </div>
+          <div id="player-ore" title="Ore">
+            <img src={OrePNG} style={pngIconStyle} alt="player ore amount" id="ore-png"/>
+            <p>1</p>
+          </div>
+        </div>
+
+        {/* --------------------- Player Other Metadata -------------------- */}
+        <div id="player-other-metadata">
+          <p style={{textDecoration: 'underline'}}>Metadata</p>
+          <div id="player-last-action" title="Last action">
+            <img src={LastActionPNG} style={pngIconStyle} alt="player last action" />
+            <p>END_TURN</p>
+          </div>
+          <div id="player-robbers-played" title='Knights played'>
+            <img src={KnightPNG} style={pngIconStyle} alt="player number of robbers played" />
+            <p>0</p>
+          </div>
+          <div id="player-longest-road" title='Longest road'>
+            <img src={LongestRoadPNG} style={pngIconStyle} alt="player longest road" />
+            <p>No</p>
+          </div>
+          <div id="player-largest-army" title='Largest army'>
+            <img src={ArmyPNG} style={pngIconStyle} alt="player number of robbers played" />
+            <p>No</p>
+          </div>
+        </div>
       </div>
-      <p style={{height: '100%'}}>Last Action: END_TURN</p>
     </div>
   )
 }
