91ba849b20fa80ba8d9e7230edc48c1d0eb47087
taylor-cox <taylor@coxintl.com>
2023-10-10 11:19:20 -0400

    made websocket sessions a map so multiple sessions can connect

--- src/main/java/com/catanai/server/config/GameSocketHandler.java
+++ src/main/java/com/catanai/server/config/GameSocketHandler.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -16,7 +17,7 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component
 public class GameSocketHandler extends TextWebSocketHandler {
   List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
-  Map<String, SocketCommandHandler> sessionCommandHandlerMap = Map.of();
+  Map<String, SocketCommandHandler> sessionCommandHandlerMap = new HashMap<>();
 
 
   @Override
