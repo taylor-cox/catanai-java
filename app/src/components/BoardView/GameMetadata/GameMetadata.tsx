67eb1461f275f15b2b13a02b2804fe868c5bb2d7
taylor-cox <taylor@coxintl.com>
2023-10-10 16:02:14 -0400

    formatting, added current action number to meta

--- app/src/components/BoardView/GameMetadata/GameMetadata.tsx
+++ app/src/components/BoardView/GameMetadata/GameMetadata.tsx
@@ -1,48 +1,50 @@
-import React from 'react';
-import './GameMetadata.css';
-import { useAppSelector } from '../../../hooks';
+import React from "react";
+import "./GameMetadata.css";
+import { useAppSelector } from "../../../hooks";
 
 const GameMetadata: React.FC = () => {
-  const currentGameState = useAppSelector((state) => state.currentGameState.value);
+  const currentGameState = useAppSelector(
+    (state) => state.currentGameState.value
+  );
   const gameStates = useAppSelector((state) => state.gameStates.value);
   let isLoading: boolean = gameStates.length === 0;
 
   const getActionString = (action: number) => {
     switch (action) {
       case 0:
-        return 'New Game';
+        return "New Game";
       case 1:
-        return 'Play Road';
+        return "Play Road";
       case 2:
-        return 'Play Settlement';
+        return "Play Settlement";
       case 3:
-        return 'Play City';
+        return "Play City";
       case 4:
-        return 'Play Knight';
+        return "Play Knight";
       case 5:
-        return 'Play Road Building';
+        return "Play Road Building";
       case 6:
-        return 'Play Year of Plenty';
+        return "Play Year of Plenty";
       case 7:
-        return 'Play Monopoly';
+        return "Play Monopoly";
       case 8:
-        return 'Draw Development Card';
+        return "Draw Development Card";
       case 9:
-        return 'Offer Trade';
+        return "Offer Trade";
       case 10:
-        return 'Accept Trade';
+        return "Accept Trade";
       case 11:
-        return 'Decline Trade';
+        return "Decline Trade";
       case 12:
-        return 'Move Robber';
+        return "Move Robber";
       case 13:
-        return 'Discard';
+        return "Discard";
       case 14:
-        return 'End Turn';
+        return "End Turn";
       case 15:
-        return 'Roll Dice';
+        return "Roll Dice";
     }
-  }
+  };
 
   if (isLoading) {
     return <div></div>;
@@ -55,15 +57,18 @@ const GameMetadata: React.FC = () => {
       </div>
       <div className="row">
         <p>Last Dice Roll: {gameStates[currentGameState].lastRoll}</p>
-        <p>Action Performed: {getActionString(gameStates[currentGameState].actionID)}</p>
+        <p>
+          Action Performed:{" "}
+          {getActionString(gameStates[currentGameState].actionID)}
+        </p>
         <p>Current Player: {gameStates[currentGameState].currentPlayer}</p>
       </div>
       <div className="row">
         <p>Total Actions: {gameStates.length}</p>
-        <p>Total Turns: {'???'}</p>
+        <p>Current Action: {currentGameState + 1}</p>
       </div>
     </div>
   );
-}
+};
 
 export default GameMetadata;
